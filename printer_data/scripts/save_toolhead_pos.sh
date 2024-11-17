da860e53105c2f1aecd7c37db502742a1daca16b
0xdreadnaught <brianrpeters@gmail.com>
2024-11-17 02:47:50 +0000

    New backup from 11/17/2024 - 02:47:50 AM

--- printer_data/scripts/save_toolhead_pos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Debug for console
-echo "Received X:$1 Y:$2 Z:$3"
-# Make sure the file exists
-touch /tmp/toolhead_position.txt
-# Save coords
-echo "$1 $2 $3" > /tmp/toolhead_position.txt
